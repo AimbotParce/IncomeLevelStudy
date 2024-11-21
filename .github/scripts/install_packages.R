0dc738b91008147083940395aa19b12e877382c4
AimbotParce <marcparcerisa@gmail.com>
2024-11-21 15:23:11 +0100

    Workflow

---
+++ .github/scripts/install_packages.R
@@ -0,0 +1,14 @@
+required_packages <- c(
+    "crayon", "car", "FactoMineR", "chemometrics", "corrplot", "readr",
+    "RColorBrewer", "PerformanceAnalytics", "mice", "dplyr", "rmarkdown"
+)
+
+install_if_missing <- function(packages) {
+    for (pkg in packages) {
+        if (!requireNamespace(pkg, quietly = TRUE)) {
+            install.packages(pkg, repos = "https://cloud.r-project.org/")
+        }
+    }
+}
+
+install_if_missing(required_packages)
